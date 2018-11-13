f4a383ed3b80a320f4f1b43fe84585341735279c
Michael Lafosse <s188037@students.aie.edu>
2018-11-12 18:01:00 -0600

    need to fix my shit

--- project2D/BinaryTree.cpp
+++ project2D/BinaryTree.cpp
@@ -28,8 +28,7 @@ void BinaryTree::insert(int a_nValue)
 	{
 		//this makes a new node then assigns root to the newd pointer.
 		//setting the value as the root.
-		TreeNode* newd = new TreeNode(a_nValue);
-		m_pRoot = newd;
+		m_pRoot = new TreeNode(a_nValue);
 		return;
 	}
 
@@ -45,6 +44,7 @@ void BinaryTree::insert(int a_nValue)
 		//there isn't anything there yet.
 		if (a_nValue < currentNode->getData())
 		{
+			//this makes a new node to be inserted.
 			TreeNode* newd = new TreeNode(a_nValue);
 			parentNode = currentNode;
 			currentNode = currentNode->getLeft();
@@ -78,29 +78,91 @@ void BinaryTree::insert(int a_nValue)
 
 void BinaryTree::remove(int a_nValue)
 {
+	TreeNode* currentNode = m_pRoot;
+	TreeNode* parentNode = m_pRoot;
+
+	TreeNode** ppOutCurrent = &currentNode;
+	TreeNode** ppOutParent = &parentNode;
+
+	//currentNode = *ppOutCurrent;
+	//parentNode = *ppOutParent;
+
+	if (findNode(a_nValue, ppOutCurrent, ppOutParent));
+	{
+
+		if (currentNode->hasRight())
+		{
+			TreeNode* ptrCurrent;
+			TreeNode* ptrParent;
+
+			ptrCurrent = *ppOutCurrent;
+			ptrParent = *ppOutParent;
+
+			while (ptrCurrent->hasLeft())
+			{
+				ptrParent = ptrCurrent;
+				ptrCurrent = ptrCurrent->getLeft();
+			}
+
+			currentNode->setData(ptrCurrent->getData());
+
+			if (ptrParent->getLeft() == ptrCurrent)
+			{
+				ptrParent->setLeft(ptrCurrent->getRight());
+			}
+
+			else if (ptrParent->getRight() == ptrCurrent)
+			{
+				ptrParent->setRight(ptrCurrent->getRight());
+			}
+
+			delete ptrCurrent;
+		}
+		else
+		{
+			if (parentNode->getLeft() == currentNode)
+			{
+				parentNode->setLeft(currentNode->getLeft());
+			}
+			else if (parentNode->getRight() == currentNode)
+			{
+				parentNode->setRight(currentNode->getLeft());
+			}
+			else if (m_pRoot == currentNode)
+			{
+				m_pRoot = currentNode->getLeft();
+			}
+			delete currentNode;
+		}
+	}
 }
 
 TreeNode * BinaryTree::find(int a_nValue)
 {
 	TreeNode* currentNode;
 	TreeNode* parentNode;
+
 	currentNode = m_pRoot;
 	parentNode = m_pRoot;
+
 	while (currentNode != nullptr)
 	{
 		if (a_nValue < currentNode->getData())
 		{
-			TreeNode* newd = new TreeNode(a_nValue);
-			parentNode = currentNode;
+			if (currentNode->hasLeft())
+			{
 			currentNode = currentNode->getLeft();
 			continue;
+			}
 		}
 		if (a_nValue > currentNode->getData())
 		{
-			TreeNode* newd = new TreeNode(a_nValue);
+			if (currentNode->hasRight())
+			{
 			parentNode = currentNode;
 			currentNode = currentNode->getRight();
 			continue;
+			}
 		}
 		if (a_nValue == currentNode->getData())
 		{
@@ -116,10 +178,29 @@ void BinaryTree::draw(aie::Renderer2D * renderer, aie::Font* g_systemFont, TreeN
 
 bool BinaryTree::findNode(int a_nSearchValue, TreeNode ** ppOutNode, TreeNode ** ppOutParent)
 {
-	TreeNode* currentNode;
+	/*TreeNode* currentNode;
 	TreeNode* parentNode;
 
 	currentNode = m_pRoot;
+	parentNode = m_pRoot;*/
+
+	while(*ppOutNode != nullptr)
+	{
+		if (a_nSearchValue == (*ppOutNode)->getData())
+		{
+			return true;
+		}
+		else if (a_nSearchValue < (*ppOutNode)->getData())
+		{
+			*ppOutParent = *ppOutNode;
+			*ppOutNode = (*ppOutNode)->getLeft();
+		}
+		else if (a_nSearchValue > (*ppOutNode)->getData())
+		{
+			*ppOutParent = *ppOutNode;
+			*ppOutNode = (*ppOutNode)->getRight();
+		}
+	}
 	return false;
 }
 
